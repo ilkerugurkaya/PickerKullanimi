27e2d337ce2b19736f0fc20cb622510d6d0a8972
ilkerugurkaya <kayailkerugur@gmail.com>
2022-12-09 13:44:21 +0300

    Initial Commit

---
+++ PickerKullanimi/PickerKullanimiApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickerKullanimiApp.swift
+//  PickerKullanimi
+//
+//  Created by İlker Kaya on 9.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct PickerKullanimiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
